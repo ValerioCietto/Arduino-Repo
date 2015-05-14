2663a981f4a5d1eb917351d97956322efd828f6f
Valerio Cietto <valerio_ciet@hotmail.it>
2015-05-14 21:13:30 +0200

    Create filtro-sensori.ino

---
+++ filtro-sensori.ino
@@ -0,0 +1,21 @@
+
+int previousValue;
+int currentValue;
+
+int noise = 10;
+
+void setup() {
+  previousValue = analogRead(0);
+  currentValue = previousValue;
+  expectedValue = previousValue;
+}
+
+void loop() {
+  currentValue = analogRead(0);
+  if( abs(currentValue-previousValue) > noise){
+    previousValue=currentValue;  
+  }
+  else{
+    currentValue=previousValue; 
+  }  
+}
